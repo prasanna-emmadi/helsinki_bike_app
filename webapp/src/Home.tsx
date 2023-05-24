64fbe9242e80656f29a813c735975cb671401c6b
Prasanna_emmadi <prasanna.177@gmail.com>
2023-05-24 23:39:29 +0300

    Add journeys, bicyclestations links in Home page

--- webapp/src/Home.tsx
+++ webapp/src/Home.tsx
@@ -1,5 +1,24 @@
+import { Heading, ListItem, OrderedList } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <>
+            <Heading>Home</Heading>
+            <OrderedList>
+                <ListItem>
+                    <Link to={"/journeys"}>
+                        Journeys
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link to={"/bicyclestations"}>
+                        BicycleStations
+                    </Link>
+                </ListItem>
+            </OrderedList>
+        </>
+    )
 }
 
 export default Home;
